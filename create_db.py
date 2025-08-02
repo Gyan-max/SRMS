fc2c9bf3f1fadeb195833bea293487ff69e7efa2
Gyan <gyan_2312res274@iitp.ac.in>
2025-08-02 18:08:31 +0530

    connecting darabase

---
+++ create_db.py
@@ -0,0 +1,9 @@
+import sqlite3
+
+def create_database():
+    conn = sqlite3.connect(database="rms.db")
+    cur = conn.cursor()
+    cur.execute("CREATE TABLE IF NOT EXISTS course (id INTEGER PRIMARY KEY AUTOINCREMENT, CourseName TEXT, Duration TEXT, Fee TEXT, Description TEXT)")
+    conn.commit()
+
+create_database()
